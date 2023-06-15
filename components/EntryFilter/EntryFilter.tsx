8fba2d0179d3a7dbd4497edf7c829d484af52e42
Felipe Faria <felipevisu@gmail.com>
2023-06-15 20:31:42 -0300

    feat: create custom select field

--- components/EntryFilter/EntryFilter.tsx
+++ components/EntryFilter/EntryFilter.tsx
@@ -16,7 +16,7 @@ export default function EntryFilter({
   const filters = buildFilters({ categories, attributes });
 
   return (
-    <div className="bg-white p-6 rounded-lg shadow-sm flex flex-col gap-4">
+    <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-sm flex flex-col gap-4">
       {filters.map((filter) => (
         <FilterItem key={filter.parameter} filter={filter} />
       ))}
--- components/EntryFilter/FilterItem.tsx
+++ components/EntryFilter/FilterItem.tsx
@@ -1,19 +1,21 @@
 "use client";
 import { useState } from "react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import Select from "react-select";
 
 import { capitalize } from "@/utils/text";
+
+import Select from "../UI/select";
 
 import { Filter } from "./utils";
 
 export default function FilterItem({ filter }: { filter: Filter }) {
+  const [selected, setSelected] = useState<string>();
   const searchParams = useSearchParams();
   const router = useRouter();
   const pathname = usePathname();
 
-  const handleChange = (e: any) => {
-    const value = e ? e.value : "";
+  const handleChange = (value: string) => {
+    setSelected(value);
     // eslint-disable-next-line compat/compat
     const params = new URLSearchParams(searchParams.toString());
     params.set(filter.parameter, value);
@@ -22,8 +24,12 @@ export default function FilterItem({ filter }: { filter: Filter }) {
 
   return (
     <div>
-      <h4 className="font-semibold mb-1">{capitalize(filter.title)}</h4>
-      <Select options={filter.choices} onChange={handleChange} isClearable />
+      <Select
+        options={filter.choices}
+        onChange={handleChange}
+        value={selected}
+        label={capitalize(filter.title)}
+      />
     </div>
   );
 }
