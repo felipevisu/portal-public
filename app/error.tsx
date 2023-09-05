df1d22464454f117afa2189f38623b2f5c32e04e
Felipe Faria <felipevisu@gmail.com>
2023-09-05 15:19:26 -0300

    feat: create 404 page

---
+++ app/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Page() {
+  return (
+    <div className="text-center py-12 uppercase">
+      <h3 className="text-5xl font-semibold mb-3">404</h3>
+      <p>Não encontrado</p>
+    </div>
+  );
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Page() {
+  return <div className="text-center py-12">Não encontrado</div>;
+}
